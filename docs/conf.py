9ba9e354f8484bfff9f36610d0defd2acf7de289
James Cumby <james.cumby@ed.ac.uk>
2016-10-21 11:39:15 +0100

    Modfied Sphinx conf.py to be compatible with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages',
+#    'sphinx.ext.githubpages',
 ]
 
 #####################################################################################
